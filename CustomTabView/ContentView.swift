861a73a46ce2eb3f2d33295d894c51894e7baa5e
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-01-27 02:34:21 +0330

    Change views with TabBar

--- CustomTabView/ContentView.swift
+++ CustomTabView/ContentView.swift
@@ -11,7 +11,22 @@ struct ContentView: View {
     @State var index = 0
     var body: some View {
         VStack(spacing: 0) {
-
+            ZStack{
+                switch index {
+                case 0:
+                    Color.red
+                case 1:
+                    Color.yellow
+                case 2:
+                    Color.green
+                case 3:
+                    Color.blue
+                default:
+                    Color.white
+                }
+            }
+            .padding(.bottom, -35)
+            .ignoresSafeArea()
             CustomTabs(index: $index)
         }
     }
