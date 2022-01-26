d00ffe7e17b49069e4917f561b5b533b82559ed6
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-01-27 02:11:35 +0330

    Added TabBar UI

--- CustomTabView/ContentView.swift
+++ CustomTabView/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var index = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            
+            CustomTabs(index: $index)
+        }
+        .background(Color.black
+                        .opacity(0.05)
+                        .edgesIgnoringSafeArea(.top)
+        )
     }
 }
 
@@ -19,3 +27,67 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct CustomTabs: View {
+    
+    @Binding var index: Int
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                index = 0
+            }) {
+                Image(systemName: "house.fill")
+
+            }
+            .foregroundColor(.black.opacity(index == 0 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {
+                index = 1
+            }) {
+                Image(systemName: "magnifyingglass")
+            }
+            .foregroundColor(.black.opacity(index == 1 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {
+            }) {
+                ZStack {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.white)
+                }
+                .frame(width: 50, height: 50)
+                .background(Color("darkBlue"))
+                .clipShape(Circle())
+            }
+            .offset(y: -25)
+            .foregroundColor(.black.opacity(index == 2 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {
+                index = 2
+            }) {
+                Image(systemName: "heart.fill")
+            }
+            .foregroundColor(.black.opacity(index == 2 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {
+                index = 3
+            }) {
+                Image(systemName: "person.fill")
+            }
+            .foregroundColor(.black.opacity(index == 3 ? 1 : 0.2))
+        }
+        .padding(.horizontal, 35)
+        .background(Color.white)
+    }
+}
+
