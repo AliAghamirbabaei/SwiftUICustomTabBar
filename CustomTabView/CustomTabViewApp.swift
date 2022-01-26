d26bc0d8ce9a5ea059a899ea05c9cfdb69ac5ec3
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-01-27 01:25:51 +0330

    Initial Commit

---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by Ali Aghamirbabaei on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
