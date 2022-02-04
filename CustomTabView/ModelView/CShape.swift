b57f879156ab4d93d4e3deb82990b683eff34474
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-02-04 22:58:33 +0330

    Fix Bug

--- CustomTabView/ModelView/CShape.swift
+++ CustomTabView/ModelView/CShape.swift
@@ -16,8 +16,7 @@ struct CShape: Shape {
             path.addLine(to: CGPoint(x: 0, y: rect.height))
             path.addLine(to: CGPoint(x: rect.width, y: rect.height))
             path.addLine(to: CGPoint(x: rect.width, y: 35))
-            
-            path.addArc(center: CGPoint(x: (rect.width / 2) + 3, y: 35), radius: 35, startAngle: .zero, endAngle: .init(degrees: 180), clockwise: true)
+            path.addArc(center: CGPoint(x: (rect.width) / 2, y: 35), radius: 35, startAngle: .zero, endAngle: .init(degrees: 180), clockwise: true)
         }
     }
 }
